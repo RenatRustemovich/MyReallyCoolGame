9fe32f177ac25723ab0313032fe235743eda0fd3
Renat Rustemovich <renatsabirov789@gmail.com>
2018-08-23 12:26:58 +0300

    my commit

---
+++ Assets/Scripts/Nodes/Node1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Node1 : MonoBehaviour {
+
+    public NodeDialog1 NodeDialog1;
+    public GameObject[] buildings;
+    private GameObject own = null;
+
+    private void OnMouseDown()
+    {
+        if(own == null) { NodeDialog1.Show(this); }
+       
+    }
+
+    public void Build(string txt)
+    {
+        switch (txt)
+        {
+            case "lemonade": own = Instantiate(buildings[0], this.transform.position, Quaternion.identity); break;
+            case "hotdogs": own = Instantiate(buildings[1], this.transform.position, Quaternion.identity); break;
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/Nodes/NodeDialog1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeDialog1 : MonoBehaviour {
+   
+    public GameObject panel1;
+    private Node1 temp;
+    public void Show(Node1 node)
+    {
+        panel1.SetActive(true);
+        temp = node;
+    }
+
+    public void Build(string txt)
+    {
+        panel1.SetActive(false);
+        temp.Build(txt);
+    }
+}
